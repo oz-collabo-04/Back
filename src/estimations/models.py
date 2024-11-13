09ebd8620b27c285b8f3307638a5fa0dbded7074
Donggurii._.dd <ehdgur741@gmail.com>
2024-11-13 14:31:01 +0900

    ✨ feat: app 별 모델 생성 및 슈퍼유저 생성
    - 이유 :
    users,
    expert,
    estimations,
    notifications,
    reservations,
    reviews,
    chat 의 모델을 생성
    common/constants/choices.py 생성

--- src/estimations/models.py
+++ src/estimations/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+from common.constants.choices import (
+    AREA_CHOICES,
+    BOOKING_STATUS_CHOICES,
+    GENDER_CHOICES,
+    SERVICE_CHOICES,
+)
+from expert.models import Expert
+from users.models import User
+
+
+class Estimation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
+    service_list = MultiSelectField(choices=SERVICE_CHOICES, max_length=10)
+    location = MultiSelectField(choices=AREA_CHOICES, max_length=30, max_choices=3)
+    due_date = models.DateField()
+    discount = models.PositiveIntegerField(null=True, blank=True)
+    total_price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class EstimationsRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service_list = MultiSelectField(choices=SERVICE_CHOICES, max_length=10)
+    prefer_gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
+    location = MultiSelectField(choices=AREA_CHOICES, max_length=30, max_choices=3)
+    status = models.CharField(max_length=20, choices=BOOKING_STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- src/notifications/models.py
+++ src/notifications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from common.constants.choices import NOTIFICATION_TYPE_CHOICES
+from users.models import User
+
+
+class Notification(models.Model):
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    message = models.CharField(max_length=100)
+    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPE_CHOICES)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
