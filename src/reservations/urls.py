0b1d0a60ec5f4c1d78a32103ed3d0e903b8533a9
kahyeonseo <kahyeonseo0330@gmail.com>
2024-11-20 11:40:25 +0900

    ✨ feat: 시리얼라이저 작성, Reservation Create, List, Retrieve, Update View 작성 및 url 매핑, 테스트 코드 작성
    - reservations/serializers.py
        - UserInfoSerializer
        - ExpertInfoSerializer
        - EstimationInfoSerializer
        - ReservationInfoSerializer
    
    - reservations/views.py
        - ReservationListAPIView
        - ReservationCreateAPIView
        - ReservationRetrieveUpdateAPIView
    
    - reservations/urls.py
        - "", views.ReservationListAPIView.as_view(), name="reservation-list"
        - "create/", views.ReservationCreateAPIView.as_view(), name="reservation-create"
        - "<int:reservation_id>/", views.ReservationRetrieveUpdateAPIView.as_view(), name="reservation-retrieve-update"
    
    - reservations/tests.py
        - ReservationTestCase
    	- setUp : Create GuestUser, Expert, EstimationRequest, Estimations
    	- test_list_reservations : Test reservation-list url
    	- test_create_reservation : Test reservation-create url
    	- test_get_reservation_detail : Test reservation-retrieve-update url, get method
    	- test_update_reservation : Test reservation-retrieve-update url, patch method

--- src/reservations/urls.py
+++ src/reservations/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from reservations import views
+
+urlpatterns = [
+    path("", views.ReservationListAPIView.as_view(), name="reservation-list"),
+    path("create/", views.ReservationCreateAPIView.as_view(), name="reservation-create"),
+    path("<int:reservation_id>/", views.ReservationRetrieveUpdateAPIView.as_view(), name="reservation-retrieve-update"),
+]
