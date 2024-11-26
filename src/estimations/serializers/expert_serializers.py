b24970753732d5bc07b5cda9cdf1f2d30fb5166f
raphaehell <raphaehell@gmail.com>
2024-11-26 23:36:47 +0900

    apply black, isort

--- src/estimations/serializers/expert_serializers.py
+++ src/estimations/serializers/expert_serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from estimations.models import Estimation
+from common.constants.choices import SERVICE_CHOICES
+from estimations.models import Estimation, RequestManager, EstimationsRequest
+from users.models import User
 
 
 # 유저의 요청에 대한 전문가의 견적 등록 요청
@@ -17,3 +19,27 @@ class EstimationUpdateByExpertSerializer(serializers.ModelSerializer):
         model = Estimation
         fields = ["id", "request", "expert", "due_date", "service", "charge", "created_at", "updated_at"]
         read_only_fields = ("id", "request", "expert", "created_at", "updated_at")
+
+
+class RequestUserSerializerForExpert(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "phone_number", "name"]
+
+
+class EstimationRequestSerializerForExpert(serializers.ModelSerializer):
+    user = RequestUserSerializerForExpert(read_only=True)
+    service_list = serializers.MultipleChoiceField(choices=SERVICE_CHOICES, read_only=True)
+
+    class Meta:
+        model = EstimationsRequest
+        fields = "__all__"
+
+
+class EstimationRequestListForExpertSerializer(serializers.ModelSerializer):
+    request = EstimationRequestSerializerForExpert(read_only=True)
+
+    class Meta:
+        model =  RequestManager
+        fields = ["id", "expert", "request", "created_at", "updated_at"]
+        read_only_fields = ("id", "expert", "request", "created_at", "updated_at")
