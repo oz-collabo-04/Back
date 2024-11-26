ea73b9d0aaacfef8bd9810240cd14149ab9cdb64
머용 <153697857+Meoyoug@users.noreply.github.com>
2024-11-26 09:49:00 +0900

    Revert "[Hotfix] 커밋 꼬임 해결"

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd ..
+
+set -a
+source prod.env
+set +a
+
+export NCLOUD_ACCESS_KEY=ncp_iam_BPAMKR3ymzryX8dUtHrP
+export NCLOUD_SECRET_KEY=ncp_iam_BPKMKRZDUzp5M16sdMxG8UaL2MEXhYGsb3
+export NCLOUD_API_GW=https://ncloud.apigw.ntruss.com
+
+docker login stop.kr.ncr.ntruss.com -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+docker build --platform linux/amd64 -t stop.kr.ncr.ntruss.com/oz-collabo-repo:latest .
+docker tag stop.kr.ncr.ntruss.com/oz-collabo-repo stop.kr.ncr.ntruss.com/oz-collabo-repo:latest
+
+docker push stop.kr.ncr.ntruss.com/oz-collabo-repo
+
+cd kubernetes
+
+KUBECONFIG_PATH=$(realpath kubeconfig.yaml)
+export KUBECONFIG=$KUBECONFIG_PATH
+
+kubectl create secret docker-registry amuguna \
+  --docker-server=stop.kr.ncr.ntruss.com \
+  --docker-username=$DOCKER_USERNAME \
+  --docker-password=$DOCKER_PASSWORD \
+  --docker-email=$DOCKER_EMAIL
+
+kubectl apply -f django_deployment.yaml
+
+kubectl rollout restart deployment django-deployment
+
+cd ..
