781702e9cd6cb2430716aa209bb90f903410ba8b
Donggurii._.dd <ehdgur741@gmail.com>
2024-12-06 13:57:47 +0900

    # 커밋 메시지 템플릿 # 아래 Gitmoji 규칙을 참고하여 커밋 메시지를 작성하세요.
    # ✨ feat: 새로운 기능 추가
    💡 chore: chat 코드 수정
    # 📝 docs: 문서 수정 (README 수정 시)
    # 🚚 build: 빌드 관련 파일 수정 및 삭제한 경우
    # ♻️ refactor: 코드 리팩터링
    # 🚑 hotfix: 긴급 수정
    
    # 제목 (필수)
    # 이모지 및 타입을 추가하여 50자 이내로 변경사항을 간단하게 요약하세요.
    # 예: ✨ feat: 새로운 로그인 기능 추가
    
    본문
    # 변경한 이유를 상세하게 설명하세요. 여러 줄일 경우, 각 줄의 길이는 72자를 넘지 않게 작성하세요.
    # 예시:
    - 이유 1 : chat/ urls.py 코드 수정
    
    # Footer (선택)
    # 관련된 이슈 번호나 참고 정보를 추가하세요.
    # 예시: Resolves: #123
    정

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from chat.views.chat_views import (
     ChatRoomDetailAPIView,
     ChatRoomListCreateAPIView,
-    ChatRoomUpdateAPIView,
     MessageListCreateAPIView,
 )
 
@@ -12,8 +11,6 @@ urlpatterns = [
     path("chatrooms/", ChatRoomListCreateAPIView.as_view(), name="chatroom-list-create"),
     # 채팅방 상세 조회 및 삭제
     path("chatrooms/<int:room_id>/", ChatRoomDetailAPIView.as_view(), name="chatroom-detail"),
-    # 채팅방 나가기 - 상대방이 존재할 때 업데이트
-    path("chatrooms/<int:room_id>/leave/", ChatRoomUpdateAPIView.as_view(), name="chatroom-update"),
     # 메시지 목록 조회 및 생성
     path("chatrooms/<int:room_id>/messages/", MessageListCreateAPIView.as_view(), name="message-list-create"),
 ]
